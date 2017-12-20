bf8b54f669d61ba50b60eda1b2ff87718b0711a8
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2017-12-19 19:37:41 -0500

    added tasks for copy fonts and to build css

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,32 @@
 const gulp = require('gulp'),
   sass = require('gulp-sass'),
   del = require('del'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer'),
   rename = require('gulp-rename'),
   cleanCSS = require('gulp-clean-css');
-
-const processors = [
-  autoprefixer({
-    browsers: ['> 1%'],
-    cascade: false
-  })
-];
 
 gulp.task('cleanup-css', () => {
   return del(['dist/css']);
 });
 
+gulp.task('cleanup-fonts', () => {
+  return del(['dist/fonts']);
+});
+
 gulp.task('css', ['cleanup-css'] , () => {
   return gulp.src('src/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss(processors))
     .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('copy-fonts', ['cleanup-fonts'], () => {
+  return gulp.src('src/fonts/*.*')
+    .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('css-build', ['cleanup-css'], () => {
   return gulp.src('src/*.scss')
+    .pipe(gulp.dest('dist/css'))
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss(processors))
     .pipe(gulp.dest('dist/css'))
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
@@ -38,5 +37,5 @@ gulp.task('watch', () => {
   gulp.watch('src/*.scss', ['css']);
 });
 
-gulp.task('build', ['css-build']);
+gulp.task('build', ['css-build', 'copy-fonts']);
 gulp.task('default', ['watch']);
